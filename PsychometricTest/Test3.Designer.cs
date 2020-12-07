8673dd103e282c4d0a2f8fc3a5db504963cd05ff
Michał Sakowicz <michalsakowicz99@gmail.com>
2020-12-07 23:37:57 +0100

    test 3 with initial and main test

--- PsychometricTest/Test3.Designer.cs
+++ PsychometricTest/Test3.Designer.cs
@@ -29,7 +29,8 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.test3Label = new System.Windows.Forms.Label();
+            this.startButtonTest3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -39,28 +40,39 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.button1.Location = new System.Drawing.Point(127, 111);
+            this.button1.Location = new System.Drawing.Point(130, 160);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(143, 140);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.alarmButton_Clicked);
             // 
-            // label1
+            // test3Label
             // 
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(376, 62);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Wcisnij przycisk natychmiast gdy uslyszysz dzwiek";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.test3Label.Location = new System.Drawing.Point(13, 13);
+            this.test3Label.Name = "test3Label";
+            this.test3Label.Size = new System.Drawing.Size(376, 74);
+            this.test3Label.TabIndex = 1;
+            this.test3Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startButtonTest3
+            // 
+            this.startButtonTest3.BackColor = System.Drawing.Color.Green;
+            this.startButtonTest3.Location = new System.Drawing.Point(302, 90);
+            this.startButtonTest3.Name = "startButtonTest3";
+            this.startButtonTest3.Size = new System.Drawing.Size(87, 31);
+            this.startButtonTest3.TabIndex = 2;
+            this.startButtonTest3.Text = "Start";
+            this.startButtonTest3.UseVisualStyleBackColor = false;
+            this.startButtonTest3.Click += new System.EventHandler(this.onStart_Click);
             // 
             // Test3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(401, 359);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButtonTest3);
+            this.Controls.Add(this.test3Label);
             this.Controls.Add(this.button1);
             this.Name = "Test3";
             this.Text = "Test3";
@@ -71,6 +83,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label test3Label;
+        private System.Windows.Forms.Button startButtonTest3;
     }
 }
--- PsychometricTest/Test3.cs
+++ PsychometricTest/Test3.cs
@@ -15,11 +15,14 @@ namespace PsychometricTest
     //2-trial attempts
     public partial class Test3 : Form
     {
-        private readonly int numberOfMeasurements = 5;
+        private readonly int numberOfMainAttempts = 5;
+        private readonly int totalNumberOfAttempts = 7;
         private Stream soundFileName;
         private System.Media.SoundPlayer soundPlayer;
         private List<long> results = new List<long>();
         private long time;
+        private Boolean isTestRunning = true;
+        private Boolean firstTime = true;
 
         public Test3()
         {
@@ -30,13 +33,32 @@ namespace PsychometricTest
             InitializeComponent();
             FormBorderStyle = FormBorderStyle.FixedSingle; //restrict from resizing
             MaximizeBox = false;  //disable maximize button
+            InitializeDescriptionLabel();
+        }
 
+        private void InitializeDescriptionLabel()
+        {
+            test3Label.Text = "Znajdujesz się w fazie szkoleniowej. Po uslyszeniu dzwieku kliknij jak najszybciej w przycisk."+
+                "Proba zostanie wykonana pieciokrotnie. Kliknij przycisk " +
+                "START aby rozpoczac probe testowa.";
+        }
+
+        private void InitializeMainAttemptLabel()
+        {
+            test3Label.Text = "Znajdujesz się w testowej. Naciśnij przycisk START aby rozpoczac test.";
+        }
+
+        private void onStart_Click(object sender, EventArgs e)
+        {
             playMusic();
+            startButtonTest3.Enabled = false;
         }
 
         private async void playMusic()
         {
+            this.button1.Enabled = false;
             await Task.Delay(new Random().Next(1000, 2500));     //delay of changing colors
+            this.button1.Enabled = true;
             soundPlayer.Play();
             time = DateTimeOffset.Now.ToUnixTimeMilliseconds();
         }
@@ -52,25 +74,40 @@ namespace PsychometricTest
         {
             stopMusic();
             addToTotalMeasurements();
+
+            if (results.Count != (totalNumberOfAttempts-numberOfMainAttempts) && results.Count != totalNumberOfAttempts)
+            {
+                playMusic();
+            }
         }
 
         private void addToTotalMeasurements()
         {
-            if (results.Count >= numberOfMeasurements)
+            if (results.Count >= totalNumberOfAttempts - numberOfMainAttempts)
             {
-                Form1.globalResults[2] = avg();
-                this.Close();
-            }
-            else
-            {
-                playMusic();
+                if (results.Count >= totalNumberOfAttempts)
+                {
+                    Form1.globalResults[2] = avg();
+                    //results.ForEach(Console.WriteLine);
+                    isTestRunning = false;
+                    soundPlayer.Stop();
+                    this.Close();
+                }
+                else if (results.Count % (totalNumberOfAttempts - numberOfMainAttempts) == 0 && firstTime)
+                {
+                    InitializeMainAttemptLabel();
+                    soundPlayer.Stop();
+                    firstTime = false;
+                    button1.Enabled = false;
+                    startButtonTest3.Enabled = true;
+                }
             }
         }
 
         private long avg()
         {
             long sum = 0;
-            foreach (var result in results)
+            foreach (var result in results.Skip(Math.Max(0, results.Count() - numberOfMainAttempts)))
             {
                 sum += result;
             }
@@ -79,8 +116,10 @@ namespace PsychometricTest
 
         protected override void OnFormClosing(FormClosingEventArgs e)  //event when form closing
         {
-            base.OnFormClosing(e);
-            soundPlayer.Stop();
+            if (isTestRunning)
+            {
+                e.Cancel = true;
+            }
         }
     }
 }
